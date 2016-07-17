fc3e09e49f22d12bfbef7b34a6b1631f30713407
Ganaraja <ganaraja@gmail.com>
2016-07-17 20:34:42 +0530

    Checkpoint_3

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,6 +15,20 @@ class Kele
     @auth_token = response["auth_token"]
   end
 
+  def get_me
+    unless @auth_token.nil?
+      response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+      @user_data = JSON.parse(response.body)
+      @user_data.keys.each do |key|
+        self.class.send(:define_method, key.to_sym) do
+          @user_data[key]
+        end
+      end
+    else
+      puts "auth_token is nil"
+    end
+  end
+
   private
     def base_api_endpoint(end_point)
       "https://www.bloc.io/api/v1/#{end_point}"
